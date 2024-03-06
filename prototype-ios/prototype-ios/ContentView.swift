6e2df849c2a6e586d36710f828fa891d10997c24
shin0729 <shin.yamaguchi0801@gmail.com>
2024-03-06 15:42:42 +0900

    feat:initial commit xcodeプロジェクトを立ち上げました

---
+++ prototype-ios/prototype-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  prototype-ios
+//
+//  Created by 山口真 on 2024/03/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ prototype-ios/prototype-ios/prototype_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  prototype_iosApp.swift
+//  prototype-ios
+//
+//  Created by 山口真 on 2024/03/06.
+//
+
+import SwiftUI
+
+@main
+struct prototype_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
